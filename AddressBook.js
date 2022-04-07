7c0e4893df43f2140eafb09c7e9564520df99704
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-04-07 12:09:07 +0530

    Added UC1 to create a addressbook

---
+++ AddressBook.js
@@ -0,0 +1,10 @@
+var scan = require('prompt-sync')();
+
+var firstName = scan("Enter First Name : ");
+var lastName = scan("Enter Last Name : ");
+var address = scan("Enter Address : ");
+var city = scan("Enter City : ");
+var state = scan("Enter State : ");
+var zip = scan("Enter Zip Code : ");
+var contact = scan("Enter Phone Number : ");
+var mail = scan("Enter Email Id : ");
